2b83667365afb1318cfbd9f8eb739f81c792ae44
lys <lys219911@gmail.com>
2025-04-28 15:01:39 +0800

    feat: 图片模块

---
+++ src/utils/image.ts
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import fs from 'fs'
+
+const URL = 'https://api.siliconflow.cn/v1/images/generations'
+const API_KEY = 'sk-kcqhpqquhjwesmklkvxzthvqarrliwdmlsqwqcieousxjdtp'
+
+export const genImage = async (text: string, imageSize: string, imagePath: string) => {
+  const postBody = {
+    model: 'Kwai-Kolors/Kolors',
+    prompt: text,
+    image_size: imageSize,
+  }
+  const res = await axios.post(URL, postBody, {
+    headers: {
+      'Authorization': `Bearer ${API_KEY}`,
+      'Content-Type': 'application/json',
+    },
+  })
+  const imageUrl = res.data.images[0].url
+  const imageRes = await axios.get(imageUrl, {
+    responseType: 'arraybuffer',
+  })
+  fs.writeFileSync(imagePath, imageRes.data)
+  return imagePath
+}
